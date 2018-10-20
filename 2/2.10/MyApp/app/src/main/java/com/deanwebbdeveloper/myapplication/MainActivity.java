582575346fa5cf835197c821ad2af62cc2222e3e
deanwebbdeveloper <dean@deanwebbdeveloper.com>
2018-10-20 21:19:27 +0100

    Chapters 2.8-2.11
    Completed 2.8-2.11

---
+++ 2/2.10/MyApp/app/src/main/java/com/deanwebbdeveloper/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.deanwebbdeveloper.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void buttonClickFunction(View view) {
+
+        Log.i("Info", "Button has been clicked");
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
